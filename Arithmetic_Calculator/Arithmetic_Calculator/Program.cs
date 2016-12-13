de121f76fa6f54025701fb1f34bc1d19f83d255b
Piotr <piotr.krzysztof.wojcik@gmail.com>
2016-12-13 23:36:22 +0100

    "Division" feature completed

--- Arithmetic_Calculator/Arithmetic_Calculator/Program.cs
+++ Arithmetic_Calculator/Arithmetic_Calculator/Program.cs
@@ -11,8 +11,11 @@ namespace Arithmetic_Calculator
         static void Main(string[] args)
         {
             while (true)
-            {
+            {   
+                //Variable for loop break
+
                 bool keepLooping = true;
+
                 // User prompt first number or exit
 
                 Console.Write("Prompt first number: ");
@@ -37,7 +40,7 @@ namespace Arithmetic_Calculator
                         {
                             Environment.Exit(0);
                         }
-                                                
+
                         else if (operation == "+" || operation == "1")
                         {
                             // User prompt second number or exit
@@ -71,7 +74,7 @@ namespace Arithmetic_Calculator
                                 }
                             }
 
-                           
+
                         }
 
 
@@ -113,27 +116,63 @@ namespace Arithmetic_Calculator
 
                         // Loop of choosing the operation in case of prompt mistake  
 
+
+                        else if (operation == "/" || operation == "3")
+                        {
+                            // User prompt second number or exit
+                            while (keepLooping)
+                            {
+                                Console.Write("\nPrompt second number: ");
+                                var entry2 = Console.ReadLine();
+
+                                if (entry2.ToLower() == "quit")
+                                {
+                                    Environment.Exit(0);
+                                }
+
+                                // Program print result of an subtraction. User starts from the beginning.
+
+                                try
+                                {
+                                    double number2 = double.Parse(entry2);
+                                    double division = number1 / number2;
+                                    Console.WriteLine("Result: " + division);
+                                    keepLooping = false;
+                                    break;
+
+                                }
+
+                                // Catch exception if User prompt any character besides the number
+
+                                catch (FormatException)
+                                {
+                                    Console.WriteLine("\nThat is not valid input");
+                                }
+                            }
+
+
+                        }
+
                         else
                         {
                             Console.WriteLine("\nPlease select the operation");
                         }
-                        
                     }
-                }                           
-                
+                }
+
                 // Catch exception if User prompt any character besides the number
 
                 catch (FormatException)
                 {
 
-                   Console.WriteLine("\nThat is not valid input");
-                   continue;
+                    Console.WriteLine("\nThat is not valid input");
+                    continue;
                 }
-
-                
             }
-                    
-                                    
+                                   
+                          
+                               
+                                   
         }
     }
 }
